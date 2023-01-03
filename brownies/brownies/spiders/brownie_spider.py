3a4c4ca089e2628c58780b77085f9d0871d4e6c8
buytfdip <buytfdip@gmail.com>
2023-01-03 13:31:14 -0500

    changed log output

--- brownies/brownies/spiders/brownie_spider.py
+++ brownies/brownies/spiders/brownie_spider.py
@@ -8,13 +8,13 @@ class BrownieSpider(scrapy.Spider):
         for products in response.css('div.col-xs-6.col-sm-4.categoryContainerCross'):
             if len(products.css('span.catProdPrice').get().replace('<span class="catProdPrice">\r\n            <span>\r\n            \r\n            \r\n                    $<span>','').replace('</span>\r\n                \r\n            \r\n        </span>\r\n    </span>','')) == 5:
                 yield {
-                    'name': products.css('span.catProdName::text').get(),
-                    'price': products.css('span.catProdPrice').get().replace('<span class="catProdPrice">\r\n            <span>\r\n            \r\n            \r\n                    $<span>','').replace('</span>\r\n                \r\n            \r\n        </span>\r\n    </span>',''),
-                    'link': products.css('a').attrib['href'], 
+                    'Name': products.css('span.catProdName::text').get(),
+                    'Price': products.css('span.catProdPrice').get().replace('<span class="catProdPrice">\r\n            <span>\r\n            \r\n            \r\n                    $<span>','').replace('</span>\r\n                \r\n            \r\n        </span>\r\n    </span>',''),
+                    'Link': 'https://www.brownies.com/food-gifts/bite-size-treats'+products.css('a').attrib['href'], 
                 }
             else:
                 yield {
-                    'name': products.css('span.catProdName::text').get(),
-                    'price': 'Special Sale Price',
-                    'link': products.css('a').attrib['href'], 
+                    'Name': products.css('span.catProdName::text').get(),
+                    'Price': 'See Site for Sale Price',
+                    'Link': 'https://www.brownies.com/food-gifts/bite-size-treats'+products.css('a').attrib['href'], 
                 }                
