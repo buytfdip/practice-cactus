8a0356c2e869d983fae36e64d04a4ec166b51d5b
buytfip <buytfdip@gmail.com>
2023-01-04 08:15:32 +0000

    new phones spider output

--- Phones/Phones/spiders/info.md
+++ Phones/Phones/spiders/info.md
@@ -2,4 +2,10 @@
 
 next_page = response.css('a[aria-label="Navigate to page 2"]::attr(href)').get()
 
-all_products =
+items = response.css('a[class="focus:outline-none group md:box-border relative"]')
+
+name = response.css('h2[class="body-1-bold duration-200 line-clamp-1 md:mb-1 md:mt-0 mt-1 normal-case overflow-ellipsis overflow-hidden text-black transition-all"]::text').get().replace('\n      ','')
+
+price = response.css('span[class="body-2-bold text-black"]::text').get().replace('\n        ','').replace('  $','')
+
+link = response.css('a[class="focus:outline-none group md:box-border relative"]::attr(href)').get()
--- Phones/Phones/spiders/phones.py
+++ Phones/Phones/spiders/phones.py
@@ -7,4 +7,12 @@ class PhonesSpider(scrapy.Spider):
     start_urls = ['https://www.backmarket.com/en-us/l/smartphones/0744fd27-8605-465d-8691-3b6dffda5969']
 
     def parse(self, response):
-
+        for item in response.css('a[class="focus:outline-none group md:box-border relative"]'):
+            yield {
+                "Name": item.css('h2[class="body-1-bold duration-200 line-clamp-1 md:mb-1 md:mt-0 mt-1 normal-case overflow-ellipsis overflow-hidden text-black transition-all"]::text').get().replace('\n      ',''),
+                "Price": item.css('span[class="body-2-bold text-black"]::text').get().replace('\n        ','').replace('  $',''),
+                "Link": item.css('a[class="focus:outline-none group md:box-border relative"]::attr(href)').get(),
+            }
+        next_page = response.css('a[aria-label="Navigate to page 2"]::attr(href)').get()
+        if next_page is not None:
+            yield response.follow(next_page,callback=self.parse)
